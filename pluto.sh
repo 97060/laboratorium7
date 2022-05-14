f8ac7236647c812bb985f971f2d3c6c542c2d58e
97060 <103126350+97060@users.noreply.github.com>
2022-05-14 18:07:21 +0200

    Add files via upload

---
+++ pluto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+touch /logi/info.log
+echo "Data utworzenia kontenera: $(date)" >> /logi/info.log
+echo "Dostępna pamięć: $(grep MemTotal /proc/meminfo)" >> /logi/info.log
+echo "Limit pamięci kontenera w bajtach: $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)" >> /logi/info.log
