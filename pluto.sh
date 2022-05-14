b57edf64428c24c1f510ac0aff6cbfb4d1c5e140
97060 <103126350+97060@users.noreply.github.com>
2022-05-14 19:34:14 +0200

    Update pluto.sh

--- pluto.sh
+++ pluto.sh
@@ -3,3 +3,4 @@ touch /logi/info.log
 echo "Data utworzenia kontenera: $(date)" >> /logi/info.log
 echo "Dostępna pamięć: $(grep MemTotal /proc/meminfo)" >> /logi/info.log
 echo "Limit pamięci kontenera w bajtach: $(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)" >> /logi/info.log
+sleep infinity
